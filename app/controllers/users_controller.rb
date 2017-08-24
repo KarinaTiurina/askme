0c8d0f6eac212fe4a6e5b488202a8739ca1c652f
KarinaTiurina <rktiurina@gmail.com>
2017-08-24 10:35:47 +0300

    users + sessions controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,38 +1,57 @@
 class UsersController < ApplicationController
+  before_action :load_user, except: [:index, :create, :new]
+
+  before_action :authorize_user, except: [:index, :new, :create, :show]
+
   def index
-    @users = [
-      User.new(
-        id: 1,
-        name: 'WALL-E',
-        username: 'wall_e',
-        avatar_url: 'http://avatarki.tomsk.ru/img/11/11_2955_100_.jpg'
-      ),
-      User.new(
-        id: 2,
-        name: 'Neytiri',
-        username: 'neytiri'
-      )
-    ]
+    @users = User.all
   end
 
   def new
+    redirect_to root_url, alert: 'Вы уже залогинены' if current_user.present?
+    @user = User.new
+  end
+
+  def create
+    redirect_to root_url, alert: 'Вы уже залогинены' if current_user.present?
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to root_url, notice: 'Пользователь успешно зарегистрирован!'
+    else
+      render 'new'
+    end
   end
 
   def edit
   end
 
+  def update
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: 'Данные обновлены!'
+    else
+      render 'edit'
+    end
+  end
+
   def show
-    @user = User.new(
-      name: 'WALL-E',
-      username: 'wall_e',
-      avatar_url: 'http://avatarki.tomsk.ru/img/11/11_2955_100_.jpg'
-    )
+    @questions = @user.questions.order(created_at: :desc)
 
-    @questions = [
-        Question.new(text: 'Kак дела?', created_at: Date.parse('16.08.2017')),
-        Question.new(text: 'В чем смысл жизни?', created_at: Date.parse('16.08.2017'))
-    ]
+    @new_question = @user.questions.build
+  end
 
-    @new_question = Question.new
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation,
+                                 :name, :username, :avatar_url)
+  end
+
+  def load_user
+    @user ||= User.find params[:id]
+  end
+
+  def authorize_user
+    reject_user unless @user == current_user
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
+
   root 'users#index'
-  resources :users
+
+  resources :users, except: [:destroy]
   resources :questions
+  resources :sessions, only: [:new, :create, :destroy]
 
   get 'show' => 'users#show'
+
+  get 'sign_up' => 'users#new'
+  get 'log_out' => 'sessions#destroy'
+  get 'log_in' => 'sessions#new'
 end
