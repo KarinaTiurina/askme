c56cdf15c0b12fab674b4b0c7638ed259723a2cb
KarinaTiurina <rktiurina@gmail.com>
2017-09-24 19:10:08 +0300

    bug delete questions fixed

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,7 +1,7 @@
 class Question < ApplicationRecord
   belongs_to :user
   belongs_to :author, class_name: 'User', optional: true
-  has_many :question_tags
+  has_many :question_tags, dependent: :destroy
   has_many :tags, through: :question_tags, source: :tag
 
   validates :text, :user, presence: true
